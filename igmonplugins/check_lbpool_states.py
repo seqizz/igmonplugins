876ce80d2b22be457a1e41ac1fe57bf4cc61cbbc
Muhammad Tauseef Khan <tauseef.khan@innogames.com>
2018-10-04 17:47:56 +0200

    Enable ipv6 states check

--- igmonplugins/check_lbpool_states.py
+++ igmonplugins/check_lbpool_states.py
@@ -5,18 +5,18 @@ import subprocess
 def main():
     debug = False
     lbpools = get_lbpools()
-    states = get_states()
-    msg = ''
+    pfctl_output, err = subprocess.Popen(['sudo', 'pfctl', '-vsr'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    send_msg = ''
     if debug:
         separator = "\n"
     else:
         separator = "\27"
 
-    for nagios_service in ['check_lbpool', 'check_lbpool_6']:
-        msg += check_states(lbpools, states, nagios_service, separator)
+    for nagios_service in ['check_states_lbpool_4', 'check_states_lbpool_6']:
+        send_msg += check_states(lbpools, pfctl_output, nagios_service, separator)
 
     if debug:
-        print msg
+        print send_msg
     else:
         for monitor in ('af-monitor.admin', 'aw-monitor'):
             nsca = subprocess.Popen(
@@ -28,78 +28,83 @@ def main():
                 ],
                 stdin=subprocess.PIPE,
             )
-            nsca.communicate(msg)
+            nsca.communicate(send_msg)
 
 def get_lbpools():
     with open('/etc/iglb/iglb.json') as jsonfile:
         lbpools_obj = json.load(jsonfile)['lbpools']
     return lbpools_obj
 
-def get_states():
-   (out,err) = subprocess.Popen(['sudo', 'pfctl', '-vsr'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-   return out
-
 def get_allow_from_acls(lb_params):
     # The allow_from field of every lbpool has a number of acl objects with each having its own dedicated state_limit
-    allow_from = {}
+    ret = {}
     for acl_name, acl_params in lb_params['allow_from'].items():
-        allow_from.update({
-            acl_name: acl_params['acls'][-1].strip("acl_ipaddr")
+        ret.update({
+            acl_name: acl_params['acls'][-1].strip("acl_ipaddr_dns")
         })
-    return allow_from
+    return ret
 
-def check_states(lbpools, states, nagios_service, separator):
-    output = ''
+
+def compare_states(line, output_pfctl_list, index, lb_params, allow_from, nagios_service, exit_code):
+    cur_protocol = line.split('::')[-2].split(":")[-1]
+    cur_port = line.split('::')[-1].strip('"')
+    cur_states = output_pfctl_list[(index + 1)].strip('[] ', ).split('States:')[1].strip()
+    status = ''
+    ret = ' '
+    if cur_protocol in line:
+        if cur_port in line:
+            if not lb_params['state_limit']:
+                lb_params['state_limit'] == 1200000
+
+            if int(cur_states) >= (int(lb_params['state_limit']) * 0.85):
+                exit_code = 2
+            elif int(cur_states) >= (int(lb_params['state_limit']) * 0.70):
+                if exit_code != 2: # If already critical, then don't make it warning by other ports/acls
+                    exit_code = 1
+            elif int(cur_states) < int(lb_params['state_limit'] * 0.70):
+                if exit_code not in [1, 2]: # If already critical/warning, then don't make it ok by other ports/acls
+                    exit_code = 0
+
+            # To bind the ports to their acl objects, every port has state_limit set separately for each
+            # acl object in allow_from attribute
+            for acl_name, acl_object_id in allow_from.items():
+                if acl_object_id in line:
+                    status += '{} Port {} = {}, '.format(acl_name, cur_port, cur_states)
+            ret += status
+    return ret, exit_code
+
+
+def check_states(lbpools, pfctl_output, nagios_service, separator):
+    msg = ''
     for lbpool, lb_params in lbpools.items():
+        status_changed = False
         statuses = 'States: '
-        index = 0
         exit_code = None
-        output_pfctl = states.splitlines()
-
-        # The allow_from field of every lbpool has a number of acl objects with each having its own dedicated state_limit
+        output_pfctl_list = pfctl_output.splitlines()
         allow_from = get_allow_from_acls(lb_params)
 
-        lines_4 = [line for line in output_pfctl if lb_params['pf_label'] in line and
-                                                  not ('dns' in line) and
-                                                  not ('inet6' in line) ]
-        lines_6 = [line for line in output_pfctl if lb_params['pf_label'] in line and
-                                                  not ('dns' in line) and
-                                                  'inet6' in line]
+        for line in output_pfctl_list:
+            indx = output_pfctl_list.index(line)
+            if lb_params['pf_label'] in line and not ('dns' in line):
+                if 'inet6' not in line and nagios_service == 'check_states_lbpool_4':
+                    cmp_out, exit_code = compare_states(line, output_pfctl_list, indx, lb_params, allow_from, nagios_service, exit_code)
+                    statuses += cmp_out
+                    status_changed = True
+                elif 'inet6' in line and nagios_service == 'check_states_lbpool_6':
+                    cmp_out, exit_code = compare_states(line, output_pfctl_list, indx, lb_params, allow_from, nagios_service, exit_code)
+                    statuses += cmp_out
+                    status_changed = True
 
-        if nagios_service == 'check_lbpool':
-            lines_list = lines_4
-        elif nagios_service == 'check_lbpool_6':
-            lines_list = lines_6
+        if status_changed:
+            if exit_code == 0:
+                statuses = 'Everything is ok | {}'.format(statuses)
+            elif exit_code == 1:
+                statuses = 'State limit is reaching 85% | {}'.format(statuses)
+            elif exit_code == 2:
+                statuses = 'State limit has reached 85% | {}'.format(statuses)
 
-        # Some lbpools have only ipv4 while others can have only ipv6 in future
-        if lines_list:
-            for line in lines_list:
-                cur_protocol = line.split('::')[-2].split(":")[-1]
-                cur_port = line.split('::')[-1].strip('"')
-                cur_states = output_pfctl[(index+1)].strip('[] ',).split('States:')[1].strip()
-                status = ''
-                if cur_protocol in line:
-                    if cur_port in line:
-                        if not lb_params['state_limit']:
-                            lb_params['state_limit'] == 1200000
-                        else:
-                            if int(cur_states) < int(lb_params['state_limit']):
-                                # If there was a state_limits reached on one of the previously checked ports of this lbpool,
-                                # don't mark this lbpool as OK no matter if this current port has less states than limit
-                                if exit_code != 2:
-                                    exit_code = 0
-                            elif cur_states >= lb_params['state_limit']:
-                                exit_code = 2
-
-                            # To bind the ports to their acl objects, every port has state_limit set separately for each
-                            # acl object in allow_from attribute
-                            for acl_name, acl_object_id in allow_from.items():
-                                if acl_object_id in line:
-                                    status += '{} Port {} = {}, '.format(acl_name, cur_port, cur_states)
-                        statuses += status
-                index += 1
-            output += ('{}\t{}\t{}\t{}{}').format(lbpool, nagios_service, exit_code, statuses, separator)
-    return output
+            msg += ('{}\t{}\t{}\t{}{}').format(lbpool, nagios_service, exit_code, statuses, separator)
+    return msg
 
 if __name__ == "__main__":
     main()
